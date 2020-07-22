263cb7dcf00915ececb0edaf21265852d1008a6b
CarlosArch <carlos_arch@hotmail.com>
2020-07-22 18:22:00 -0500

    Reordering & Gradient test

--- code/win32_handmade.c
+++ code/win32_handmade.c
@@ -1,12 +1,39 @@
 #include <windows.h>
+#include <stdint.h>
 
 #define internal static
 #define persistent static
+#define global static
+
+typedef int8_t   int8;
+typedef int16_t  int16;
+typedef int32_t  int32;
+typedef int64_t  int64;
+typedef uint8_t  uint8;
+typedef uint16_t uint16;
+typedef uint32_t uint32;
+typedef uint64_t uint64;
+
+global uint8 quit;
+global BITMAPINFO bitmap_info;
+global void *bitmap_memory;
+global int bitmap_w;
+global int bitmap_h;
+global int bytes_per_pixel;
+
+typedef struct {
+    uint8 blue;
+    uint8 green;
+    uint8 red;
+    uint8 padding;
+} single_pixel;
 
 /* Prototypes */
 int WinMain(HINSTANCE, HINSTANCE, LPSTR, int);
 LRESULT main_window_behavior(HWND, UINT, WPARAM, LPARAM);
-internal void resize_bitmap_section(void);
+internal void allocate_bitmap(int, int);
+internal void update_window(HDC, RECT*);
+internal void show_gradient(int, int);
 
 /* Functions */
 int WinMain(
@@ -15,16 +42,16 @@ int WinMain(
         LPSTR command_line,
         int showcode)
 {
+    HWND window_handle;
     { // Window Creation
-        WNDCLASSEX main_window_class = {0};
-        { // Definition
+        WNDCLASSEX main_window_class; { // Definition
             main_window_class.cbSize = sizeof(WNDCLASSEX);
             main_window_class.style = CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
             main_window_class.lpfnWndProc = main_window_behavior;
             main_window_class.hInstance = instance;
             main_window_class.lpszClassName = "main_window";
-            main_window_class.hIcon         = 0; // TODO: Add icons
-            main_window_class.hIconSm       = 0; // TODO: Add icons
+            main_window_class.hIcon = 0; // TODO: Add icons
+            main_window_class.hIconSm = 0; // TODO: Add icons
 
             main_window_class.cbClsExtra = 0;
             main_window_class.cbWndExtra = 0;
@@ -35,7 +62,7 @@ int WinMain(
 
         RegisterClassExA(&main_window_class);
 
-        CreateWindowExA(
+        window_handle = CreateWindowExA(
             0,                                // ExStyle
             main_window_class.lpszClassName,  // Class Name
             "Handmade Tutorial",              // Window Name
@@ -53,13 +80,25 @@ int WinMain(
 
     { // Message Handling Loop
         MSG message;
-        while (GetMessageA(&message, 0, 0, 0) > 0)
+        int x = 0;
+        int y = 0;
+        while (!quit)
         {
-            TranslateMessage(&message);
-            DispatchMessageA(&message);
+            while (PeekMessageA(&message, 0, 0, 0, PM_REMOVE))
+            {
+                if (message.message == WM_QUIT)
+                    return 0;
+                TranslateMessage(&message);
+                DispatchMessageA(&message);
+            }
+            show_gradient(x, y);
+            x++; y++;
+            HDC device_context = GetDC(window_handle);
+            RECT client_rect; GetClientRect(window_handle, &client_rect);
+            update_window(device_context, &client_rect);
+            ReleaseDC(window_handle, device_context);
         }
     }
-
     return 0;
 }
 
@@ -75,17 +114,23 @@ LRESULT main_window_behavior(
     {
         case WM_CLOSE:
             OutputDebugStringA("Closing Window.\n");
-            DestroyWindow(window);
+            quit = 1;
             break;
 
         case WM_DESTROY:
             OutputDebugStringA("Destroying Window.\n");
-            PostQuitMessage(0);
+            quit = 1;
             break;
 
         case WM_SIZE:
             {
                 OutputDebugStringA("Rezising Window.\n");
+                int w, h; { // Get client size
+                    RECT client_rect; GetClientRect(window, &client_rect);
+                    w = client_rect.right - client_rect.left;
+                    h = client_rect.bottom - client_rect.top;
+                }
+                allocate_bitmap(w, h);
             } break;
 
         case WM_ACTIVATEAPP:
@@ -95,13 +140,14 @@ LRESULT main_window_behavior(
         case WM_PAINT:
         {
             PAINTSTRUCT paint;
-            HDC context = BeginPaint(window, &paint);
+            HDC device_context = BeginPaint(window, &paint);
             { // Paint process.
                 int x = paint.rcPaint.left;
                 int y = paint.rcPaint.top;
                 int w = paint.rcPaint.right - paint.rcPaint.left;
                 int h = paint.rcPaint.bottom - paint.rcPaint.top;
-                PatBlt(context, x, y, w, h, WHITENESS);
+                RECT client_rect; GetClientRect(window, &client_rect);
+                update_window(device_context, &client_rect);
             }
             EndPaint(window, &paint);
         } break;
@@ -111,6 +157,69 @@ LRESULT main_window_behavior(
             break;
     }
     return result;
+}
+
+internal void allocate_bitmap(int width, int height)
+{
+    bitmap_w = width;
+    bitmap_h = height;
+
+    { // Bitmap Info Definiiton
+        bitmap_info.bmiHeader.biSize = sizeof(BITMAPINFOHEADER);
+        bitmap_info.bmiHeader.biWidth = bitmap_w;
+        bitmap_info.bmiHeader.biHeight = -bitmap_h;
+        bitmap_info.bmiHeader.biPlanes = 1;
+        bitmap_info.bmiHeader.biBitCount = 32;
+        bitmap_info.bmiHeader.biCompression = BI_RGB;
+    }
+    bytes_per_pixel = bitmap_info.bmiHeader.biBitCount / 8;
+    int bitmap_memory_size = (bitmap_w*bitmap_h)*bytes_per_pixel;
+
+    if (bitmap_memory)
+        VirtualFree(bitmap_memory, 0, MEM_RELEASE);
+
+    bitmap_memory = VirtualAlloc(
+        0,
+        bitmap_memory_size,
+        MEM_COMMIT,
+        PAGE_READWRITE
+    );
+}
+
+internal void update_window(
+    HDC device_context,
+    RECT *window_rect)
+{
+    int window_w = window_rect->right - window_rect->left;
+    int window_h = window_rect->bottom - window_rect->top;
+    StretchDIBits(
+        device_context,
+        0, 0, bitmap_w, bitmap_h,
+        0, 0, window_w, window_h,
+        bitmap_memory,
+        &bitmap_info,
+        DIB_RGB_COLORS, SRCCOPY
+    );
+}
+
+internal void show_gradient(int x_offset, int y_offset)
+{
+    int pitch = bitmap_w*4;
+    uint8 *row = (uint8 *) bitmap_memory;
+    for(int y = 0; y < bitmap_h; ++y)
+    {
+        single_pixel *pixel = (single_pixel *) row;
+        for(int x= 0; x < bitmap_w; ++x)
+        {
+            single_pixel pix;
+            pix.blue = (uint8)(x + x_offset);
+            pix.green = (uint8)(y + y_offset);
+
+            *pixel++ = pix;
+        }
+        row += pitch;
+    }
+
 }
 
 // WinMain defines the program's entry point.
